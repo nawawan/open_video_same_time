e5cc02015b24617a60769f1473bcbfcf45f558ff
nawawan <mizutake0428@yahoo.co.jp>
2022-02-17 21:49:56 +0900

    コミット

--- public/sketch.js
+++ public/sketch.js
@@ -9,14 +9,15 @@ function setup() {
     //socket.on('match', result);
     createCanvas(1000, 500);
     noStroke();
-    vid = createVideo('asset/firenew2.mp4', vidLoad);
-    vid.size(320, 180);
-    vid2 = createVideo('asset/firenew2.mp4', vid2Load);
-    vid2.size(320, 180);
-    vid3 = createVideo('asset/firenew2.mp4', vid3Load);
-    vid3.size(320, 180);
-    vid4 = createVideo('asset/firenew2.mp4', vid4Load);
-    vid4.size(320, 180);
+    vid = createVideo('asset/tranp5.mov', vidLoad);
+    vid.size(900, 450);
+    vid.position(275, 145);
+    vid.pause();
+    let img = createImg(
+      'asset/tranp_black.png',
+    );
+    img.position(200, 200);
+    img.size(400, 380);
 }
 
 function controlkeyboard(keyboard){
@@ -24,26 +25,17 @@ function controlkeyboard(keyboard){
 }
 function vidLoad() {
   vid.elt.muted = true;
-  vid.time(5);
-  vid.loop();
-}
-function vid2Load() {
-  vid2.elt.muted = true;
-  vid2.time(9.5);
-  vid2.loop();
-}
-function vid3Load() {
-  vid3.elt.muted = true;
-  vid3.time(13.5);
-  vid3.loop();
-}
-function vid4Load() {
-  vid4.elt.muted = true;
-  vid4.loop();
 }
 let r = 100;
+let cnt = 0;
 function draw() {
   background(0);
+  if(keyIsPressed){
+    vid.play();
+  }
+  else{
+    vid.pause();
+  }
   /*if(mouseIsPressed){
     fill('green');
   }else{
